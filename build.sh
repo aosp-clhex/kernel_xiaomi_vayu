22054b97f9398481cfe6c74d8b2b2e37b2f09d84
raghavt20 <raghavtandon2000@gmail.com>
2021-04-17 23:33:03 +0530

    build.sh: Kang build script From - https://github.com/xiaomi-sm6150/android_kernel_xiaomi_sm6150/commit/e07307acdc1dfc4226b5119cba935112d3ee3d0c
     - Setup standalone kernel build script
     - Ignore out dir
     - Clone AnyKernel3

---
+++ build.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+kernel_dir="${PWD}"
+CCACHE=$(command -v ccache)
+objdir="${kernel_dir}/out"
+anykernel=$HOME/anykernel
+builddir="${kernel_dir}/build"
+ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image
+kernel_name="Rectilia"
+zip_name="$kernel_name-$(date +"%d%m%Y-%H%M").zip"
+TC_DIR=$HOME/tc/
+CLANG_DIR=$TC_DIR/clang-r458507
+export CONFIG_FILE="vayu_defconfig"
+export ARCH="arm64"
+export KBUILD_BUILD_HOST=clhexftw
+export KBUILD_BUILD_USER=home
+
+export PATH="$CLANG_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+    echo "Toolchain not found! Cloning to $TC_DIR..."
+    if ! git clone -q --depth=1 --single-branch https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b master $TC_DIR; then
+        echo "Cloning failed! Aborting..."
+        exit 1
+    fi
+fi
+
+# Colors
+NC='\033[0m'
+RED='\033[0;31m'
+LRD='\033[1;31m'
+LGR='\033[1;32m'
+
+make_defconfig()
+{
+    START=$(date +"%s")
+    echo -e ${LGR} "########### Generating Defconfig ############${NC}"
+    make -s ARCH=${ARCH} O=${objdir} ${CONFIG_FILE} -j$(nproc --all)
+}
+compile()
+{
+    cd ${kernel_dir}
+    echo -e ${LGR} "######### Compiling kernel #########${NC}"
+    make -j$(nproc --all) \
+    O=out \
+    ARCH=${ARCH}\
+    CC="ccache clang" \
+    CLANG_TRIPLE="aarch64-linux-gnu-" \
+    CROSS_COMPILE="aarch64-linux-gnu-" \
+    CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+    LLVM=1 \
+    LLVM_IAS=1
+}
+
+completion()
+{
+    cd ${objdir}
+    COMPILED_IMAGE=arch/arm64/boot/Image
+    COMPILED_DTBO=arch/arm64/boot/dtbo.img
+    if [[ -f ${COMPILED_IMAGE} && ${COMPILED_DTBO} ]]; then
+
+        git clone -q https://github.com/clhexftw/AnyKernel3 -b master $anykernel
+
+        mv -f $ZIMAGE ${COMPILED_DTBO} $anykernel
+
+        cd $anykernel
+        find . -name "*.zip" -type f
+        find . -name "*.zip" -type f -delete
+        zip -r AnyKernel.zip *
+        mv AnyKernel.zip $zip_name
+        mv $anykernel/$zip_name $HOME/$zip_name
+        rm -rf $anykernel
+        END=$(date +"%s")
+        DIFF=$(($END - $START))
+        curl --upload-file $HOME/$zip_name https://free.keep.sh; echo
+        rm $HOME/$zip_name
+        echo -e ${LGR} "############################################"
+        echo -e ${LGR} "############# OkThisIsEpic!  ##############"
+        echo -e ${LGR} "############################################${NC}"
+        exit 0
+    else
+        echo -e ${RED} "############################################"
+        echo -e ${RED} "##         This Is Not Epic :'(           ##"
+        echo -e ${RED} "############################################${NC}"
+        exit 1
+    fi
+}
+make_defconfig
+compile
+completion
+cd ${kernel_dir}
