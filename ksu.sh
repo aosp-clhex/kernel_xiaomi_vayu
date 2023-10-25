5a118483e36e156bd88aad7ba6a3d7ec20e15464
clhex <clhexftw@protonmail.com>
2023-10-25 18:57:04 +0530

    add a script to build kernel with ksu
    - enable ksu in diff defconfig while we r at it

---
+++ ksu.sh
@@ -0,0 +1,6 @@
+git fetch https://github.com/aosp-clhex/kernel_xiaomi_vayu t-r-ksu
+git cherry-pick fd68667d8733b6c2b5e4dbbe34d77ff159a0618d
+git submodule init
+git submodule update --remote --merge
+sed -i 's/vayu_defconfig/vayu_ksu_defconfig/g' build.sh
+bash b
