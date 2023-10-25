5170b49bb68056439997199a71e5637b42f8fe1c
clhex <clhexftw@protonmail.com>
2023-10-25 19:06:08 +0530

    ksu.sh: rename kernel if its a ksu build

--- ksu.sh
+++ ksu.sh
@@ -3,4 +3,5 @@ git cherry-pick fd68667d8733b6c2b5e4dbbe34d77ff159a0618d
 git submodule init
 git submodule update --remote --merge
 sed -i 's/vayu_defconfig/vayu_ksu_defconfig/g' build.sh
+sed -i 's/Rectilia/Rectilia-KSU/g' build.sh
 bash b
