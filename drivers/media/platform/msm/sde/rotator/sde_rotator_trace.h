0844e5cca67f08a56637a06e606a4212d105e422
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2025-06-02 17:40:03 +0530

    msm/sde/rotator: macro: Switch to COUNT_ARGS macro
    Signed-off-by: clhex ftw <clhexftw@protonmail.com>

--- drivers/media/platform/msm/sde/rotator/sde_rotator_trace.h
+++ drivers/media/platform/msm/sde/rotator/sde_rotator_trace.h
@@ -203,7 +203,7 @@ TRACE_EVENT(rot_bw_ao_as_context,
 
 #define SDE_ROT_TRACE_EVTLOG_SIZE	15
 TRACE_EVENT(sde_rot_evtlog,
-	TP_PROTO(const char *tag, u32 tag_id, u32 cnt, u32 data[]),
+	TP_PROTO(const char *tag, u32 tag_id, u32 cnt, u32 *data),
 	TP_ARGS(tag, tag_id, cnt, data),
 	TP_STRUCT__entry(
 			__field(int, pid)
